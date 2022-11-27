1a5a2f57a78ce5212a1a337abeb9f7025d66d2dd
Michelle Shirk <michelle.l.shirk@gmail.com>
2022-11-27 16:34:04 -0500

    finish parts 1-3

---
+++ part-2/App.js
@@ -0,0 +1,22 @@
+const App = () => (
+  <div>
+    <Tweet 
+      username="Munty"
+      name="Mr. Breakfast"
+      date="November 27, 2022"
+      message="I think it's food time!"
+    />
+    <Tweet 
+      username="Kibo"
+      name="Kibo-chan"
+      date="November 28, 2022"
+      message="I don't like you, but I agree with you."
+    />
+    <Tweet 
+      username="Racky"
+      name="Racky Raccoon"
+      date="November 29, 2022"
+      message="I'm down if you are!"
+    />
+  </div>
+)
